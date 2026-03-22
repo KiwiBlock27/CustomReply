4eda057753f15f5876c160c165c1a1d4d7261ea6
KiwiBlock27 <143954570+KiwiBlock27@users.noreply.github.com>
2026-03-22 10:40:33 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ from tgfs.utils.translation import registry, en
 
 class CustomText(en):
     START_TEXT="Hello how are you"
-
+    
+    HELP_TEXT = "This is help command"
+    
 registry["en"]=CustomText
