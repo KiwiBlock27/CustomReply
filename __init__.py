7f552574c84a9b42ceece6ca0db314c00dfb0069
KiwiBlock27 <143954570+KiwiBlock27@users.noreply.github.com>
2026-03-22 10:37:54 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,6 @@
 from tgfs.utils.translation import registry, en
 
 class CustomText(en):
-    START_TEXT="Hey there how are you\nYou can send me any file and i will generate link for that file"
-    HELP_TEXT = """
-Available Commands:
-/start - Start the bot
-/help - Show this help message
-/group - Start creating a group of files
-/done - Finish adding files to the group
-/files - List your uploaded files or created groups
-/setln - Change your language
-/cancel - Cancel the current operation
-"""
+    START_TEXT="Hello how are you"
 
 registry["en"]=CustomText
